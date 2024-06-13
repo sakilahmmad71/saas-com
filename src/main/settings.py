f0a32c0a479e0c603687dba6227604fde5ec8765
Shakil Ahmed <sakilahmmad71>
2024-06-13 12:31:26 +0600

    Prepare for railway

--- src/main/settings.py
+++ src/main/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# TODO: Need to use .env file for SECRET_KEY and other environment variables
 SECRET_KEY = "django-insecure-rg+0gxiq=5%5#q4rjp8cz+4@mj463wk=qhjm(@6=(_wr_+se80"
 
 # SECURITY WARNING: don't run with debug turned on in production!
