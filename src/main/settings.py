8e1aab345792aa5a35145d3835be3c53c3b50447
Shakil Ahmed <sakilahmmad71@gmail.com>
2024-06-14 00:55:18 +0600

    Added database and migrate the changes also added UI links and scripts as cdn

--- src/main/settings.py
+++ src/main/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from decouple import config
 from pathlib import Path
 
-import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,34 +81,19 @@ WSGI_APPLICATION = "main.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=300)
-DATABASE_URL = config("DATABASE_URL", cast=str)
+# CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=300)
+# DATABASE_URL = config("DATABASE_URL", cast=str)
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-# DATABASES = {
-#     "default": dj_database_url.config(
-#         default=DATABASE_URL,
-#         conn_max_age=CONN_MAX_AGE,
-#         conn_health_checks=True,
-#     ),
-# }
-
-if DATABASE_URL is not None:
-    import dj_database_url
-
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=CONN_MAX_AGE,
-            conn_health_checks=True,
-        ),
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
+}
 
 
 # Password validation
