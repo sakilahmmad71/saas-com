f007d60bc58196c85d1495895c3bd5f2b9a499e7
Shakil Ahmed <sakilahmmad71>
2024-06-14 18:45:05 +0600

    vendor pull command working fine now

--- src/commando/management/commands/vendor_pull.py
+++ src/commando/management/commands/vendor_pull.py
@@ -1,7 +1,10 @@
 from typing import Any
 from django.core.management.base import BaseCommand
 
+from django.conf import settings
 from helpers import download_to_local
+
+STATICFILES_VENDOR_DIR = getattr(settings, "STATICFILES_VENDOR_DIR")
 
 VENDOR_STATICFILES = {
     "flowbite.min.css": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css",
@@ -13,7 +16,30 @@ class Command(BaseCommand):
     help = "Pull the latest version of the vendor repository"
 
     def handle(self, *args: Any, **options: Any):
-        print("Pulling the latest version of the vendor repository")
+        self.stdout.write("Pulling the latest version of the vendor repository")
+        completed_urls = []
+
+        # Download each vendor file
         for filename, url in VENDOR_STATICFILES.items():
-            download_to_local(url, f"vendor/{filename}")
-            print(f"Downloaded {filename} from {url}")
+            out_path = STATICFILES_VENDOR_DIR / filename
+            download_success = download_to_local(url, out_path)
+
+            if download_success:
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Downloaded {filename} from {url} and saved to {out_path}"
+                    )
+                )
+                completed_urls.append(url)
+            else:
+                self.stdout.write(
+                    self.style.ERROR(f"Failed to download {filename} from {url}")
+                )
+
+        # Check if all vendor files were downloaded successfully
+        if set(completed_urls) == set(VENDOR_STATICFILES.values()):
+            self.stdout.write(
+                self.style.SUCCESS("All vendor files downloaded successfully")
+            )
+        else:
+            self.stdout.write(self.style.ERROR("Failed to download all vendor files"))
