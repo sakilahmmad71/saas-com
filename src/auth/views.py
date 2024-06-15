f1cca3cd15f0d584684502ec199fa07d46c1897a
Shakil Ahmed <sakilahmmad71>
2024-06-15 23:41:30 +0600

    Users can login now via auth flow

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,26 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
 def login_view(request, *args, **kwargs):
-    # Getting the username and password from the form
-    username = request.POST.get("username")
-    password = request.POST.get("password")
+    # print("🚀 ~ login_view ~ request.method", request.method)
+    # print("🚀 ~ login_view ~ request.POST", request.POST)
 
-    # Create a context dictionary to pass data to the
-    context = {"username": username, "password": password}
+    if request.method == "POST":
+        # Getting the username and password from the form
+        username = request.POST.get("username") or None
+        password = request.POST.get("password") or None
 
-    # Authenticate the user
-    user = authenticate(request, username=username, password=password)
+        if all([username, password]):
+            # Create a context dictionary to pass data to the
+            context = {"username": username, "password": password}
+            # Authenticate the user
+            user = authenticate(request, username=username, password=password)
 
-    # Check if the user is authenticated
-    if user is not None:
-        context["user"] = user
-        context["valid"] = True
-
-        # Login the user
-        login(request, user)
+            # Check if the user is authenticated
+            if user is not None:
+                print("User is authenticated")
+                # Login the user
+                login(request, user)
+                redirect("/")
 
     # return the context data to the login.html template
-    return render(request, "auth/login.html", context)
+    return render(request, "pages/login.html", {})
